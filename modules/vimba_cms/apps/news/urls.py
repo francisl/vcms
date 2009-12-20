cf612aeca82c58995fc7e338a036c6631dba8515
Francois Lebel <francoislebel@gmail.com>
2009-12-20 17:23:28 -0500

    Corrected an incomplete url name.

--- modules/vimba_cms/apps/news/urls.py
+++ modules/vimba_cms/apps/news/urls.py
@@ -19,7 +19,7 @@ urlpatterns = patterns('vimba_cms.apps.news.views',
     (r'^preview/(?P<category>[-\w]+)/$', 'Preview'),
     (r'^(?P<page>[-\w]+)/$', 'Generic'),
     (r'^(?P<year>\d{4})/$', 'Generic'),
-    (r'^all/$', 'Ge'),
+    (r'^all/$', 'Generic'),
     #(r'^$', 'Generic'),
 )
 
