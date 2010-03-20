570d2e0142e4b2343060c3691007f8f5f2482a0e
Francois Lebel <francoislebel@gmail.com>
2010-03-20 15:49:14 -0400

    Added a basic settings.py for SimpleNews.

---
+++ modules/vcms/apps/simplenews/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Application: Vimba - CMS
+# Module: SimpleNews
+# Copyright (c) 2010 Vimba inc. All rights reserved.
+# Created by Francois Lebel on 20-03-2010.
+
+from django.conf import settings
+
+
+settings.INSTALLED_APPS += (
+    'tagging', # http://code.google.com/p/django-tagging/
+)
