6313b36d3202e48a3813de248a9d651e232dded2
Francois Lebel <francoislebel@gmail.com>
2010-03-20 15:50:32 -0400

    Added initial models Article, News and NewsPageModule.

--- modules/vcms/apps/simplenews/models.py
+++ modules/vcms/apps/simplenews/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+# -*- coding: UTF-8 -*-
+# Application: Vimba - CMS
+# Module: SimpleNews
+# Copyright (c) 2010 Vimba inc. All rights reserved.
+# Created by Francois Lebel on 20-03-2010.
 
-# Create your models here.
+from django.db import models
+from tagging.models import TagField
+from vcms.apps.www.models import PageElementPosition
+
+
+class Article(models.Model):
+    LIVE_STATUS = 1
+    DRAFT_STATUS = 2
+    HIDDEN_STATUS = 3
+    STATUS_CHOICES = ((LIVE_STATUS, _('Live')), (DRAFT_STATUS, _('Draft')), (HIDDEN_STATUS, _('Hidden')))
+    date = models.DateTimeField(auto_now=True, editable=True)
+    name = models.CharField(max_length="40", help_text=_("Max 40 characters"), verbose_name=_("Title"))
+    excerpt = models.TextField(verbose_name=_("Preview"))
+    content = models.TextField()
+    language = models.ForeignKey(Language)
+    tags = TagField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS, help_text=_("Only Entries with the 'live' status will be publicly displayed."))
+
+    def __unicode__(self):
+        return self.name
+
+
+class News(Article):
+    class Meta:
+        verbose_name_plural = _("News")
+        get_latest_by = ['-date']
+        ordering = ['-date']
+
+
+class NewsPageModule(PageElementPosition):
+    from vcms.apps.www.models import DashboardPage as DP
+    page = models.ForeignKey(DP)
+    tags = TagField()
+    title = models.CharField(max_length="60", help_text=_("Max 60 characters"), verbose_name=_("Title"))
+
+    def __unicode__(self):
+        return self.page.name
