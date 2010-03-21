91e9c69fbead52120881786344d6d2485a754d0a
Francois Lebel <francoislebel@gmail.com>
2010-03-20 20:47:10 -0400

    Created the initial views.

--- modules/vcms/apps/simplenews/views.py
+++ modules/vcms/apps/simplenews/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+# -*- coding: UTF-8 -*-
+# Application: Vimba - CMS
+# Module: SimpleNews
+# Copyright (c) 2010 Vimba inc. All rights reserved.
+# Created by Francois Lebel on 20-03-2010.
+
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def news_index(request, page=1):
+    return render_to_response("", {}, context_instance=RequestContext(request))
+
+def news_unique(request, news_id=1):
+    return render_to_response("", {}, context_instance=RequestContext(request))
+
+def news_category(request, category, page=1):
+    return render_to_response("", {}, context_instance=RequestContext(request))
+
+def news_archives(request, month, year, page=1):
+    return render_to_response("", {}, context_instance=RequestContext(request))
