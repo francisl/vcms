429ddcea3446ed30970680c3a5d6f7a16a8edf20
Francois Lebel <francoislebel@gmail.com>
2010-03-21 23:02:23 -0400

    Fixed the models to inherit from Page.

--- modules/vcms/apps/simplenews/models.py
+++ modules/vcms/apps/simplenews/models.py
@@ -7,31 +7,22 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from tagging.fields import TagField
-from vcms.apps.www.models import Language, PageElementPosition
+from vcms.apps.www.models import Language, Page, PageElementPosition
 
 
-class Article(models.Model):
-    LIVE_STATUS = 1
-    DRAFT_STATUS = 2
-    HIDDEN_STATUS = 3
-    STATUS_CHOICES = ((LIVE_STATUS, _('Live')), (DRAFT_STATUS, _('Draft')), (HIDDEN_STATUS, _('Hidden')))
-    date = models.DateTimeField(auto_now=True, editable=True)
-    name = models.CharField(max_length="40", help_text=_("Max 40 characters"), verbose_name=_("Title"))
-    excerpt = models.TextField(verbose_name=_("Preview"))
+APP_SLUGS = "simplenews"
+
+
+class Article(Page):
     content = models.TextField()
-    language = models.ForeignKey(Language)
     tags = TagField()
-    status = models.IntegerField(choices=STATUS_CHOICES, default=LIVE_STATUS, help_text=_("Only Entries with the 'live' status will be publicly displayed."))
-
-    def __unicode__(self):
-        return self.name
 
 
 class News(Article):
     class Meta:
         verbose_name_plural = _("News")
-        get_latest_by = ['-date']
-        ordering = ['-date']
+        get_latest_by = ['-date_created']
+        ordering = ['-date_created']
 
 
 class NewsPageModule(PageElementPosition):
