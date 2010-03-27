c7ad09d39cefd43860bf1896faf7d894443c4efa
Francois Lebel <francoislebel@gmail.com>
2010-03-26 22:15:44 -0400

    Added app SimpleAnnouncements.

---
+++ modules/vcms/apps/simpleannouncements/models.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+# Application: Vimba - CMS
+# Module: SimpleNews
+# Copyright (c) 2010 Vimba inc. All rights reserved.
+# Created by Francois Lebel on 26-03-2010.
+
+from django.db import models
+from tagging.fields import TagField
+from vcms.apps.www.models import Page
+
+
+APP_SLUGS = "simpleannouncements"
+
+
+class Announcement(Page):
+    content = models.TextField()
+    tags = TagField()
