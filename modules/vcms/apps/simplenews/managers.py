a5d5dc94d993e2f81c53970e95e1830bbc0da0bf
Francois Lebel <francoislebel@gmail.com>
2010-03-27 17:33:10 -0400

    Added a manager for the News model, and added validation to restrict the News to the users authorized as specified in the category.

---
+++ modules/vcms/apps/simplenews/managers.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+# Application: Vimba - CMS
+# Module: SimpleNews
+# Copyright (c) 2010 Vimba inc. All rights reserved.
+# Created by Francois Lebel on 27-03-2010.
+
+from django.db import models
+from django.db.models import Q
+
+
+class NewsManager(models.Manager):
+    def get_news_for_user(self, user):
+        """
+            Returns the News instances associated to the given user.
+            A superuser can view all News instances.
+        """
+        if user.is_superuser:
+            return self.all()
+        return self.filter(Q(category__authorized_users=user) | Q(category__authorized_groups__in=user.groups.all())).distinct()
