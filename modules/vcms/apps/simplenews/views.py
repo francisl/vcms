dbfca864257817f29ce850e39299e537a163da25
Francois Lebel <francoislebel@gmail.com>
2010-03-27 23:05:05 -0400

    Added the basic implementation for the news index.

--- modules/vcms/apps/simplenews/views.py
+++ modules/vcms/apps/simplenews/views.py
@@ -6,10 +6,13 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from vcms.apps.simplenews import settings
+from vcms.apps.simplenews.models import News
 
 
 def news_index(request, category_slug, page=1):
-    return render_to_response("", {}, context_instance=RequestContext(request))
+    news = News.objects.all()[:settings.MAX_NEWS_PER_PAGE]
+    return render_to_response("index.html", { "news": news }, context_instance=RequestContext(request))
 
 def news_unique(request, category_slug, news_slug):
     context = {}
