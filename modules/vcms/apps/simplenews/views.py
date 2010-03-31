01297ac8f8f52493fe5f8ab4c1cb7dd06dc73ecf
Francois Lebel <francoislebel@gmail.com>
2010-03-31 14:07:40 -0400

    Added a generic work-in-progess pagination to the news and master template.

--- modules/vcms/apps/simplenews/views.py
+++ modules/vcms/apps/simplenews/views.py
@@ -4,6 +4,8 @@
 # Copyright (c) 2010 Vimba inc. All rights reserved.
 # Created by Francois Lebel on 20-03-2010.
 
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from vcms.apps.simplenews import settings
@@ -15,9 +17,22 @@ def news_index(request, category_slug, page=1):
     if category_slug:
         news = News.objects.filter(category__slug=category_slug)
     else:
-        news = news = News.objects.all()
-    news = news[:settings.MAX_NEWS_PER_PAGE]
-    return render_to_response("index.html", { "categories": categories, "contents": news }, context_instance=RequestContext(request))
+        news = News.objects.all()
+    # Paginate the results
+    paginator = Paginator(news, settings.MAX_NEWS_PER_PAGE)
+    try:
+        news_paginator = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        news_paginator = paginator.page(paginator.num_pages)
+    contents = news_paginator.object_list
+    # _TODO: Make the following generic
+    if category_slug:
+        paginator_previous_url = reverse("vcms.apps.simplenews.views.news_index", kwargs={ "category_slug": category_slug, "page": news_paginator.previous_page_number() })
+        paginator_next_url = reverse("vcms.apps.simplenews.views.news_index", kwargs={ "category_slug": category_slug, "page": news_paginator.next_page_number() })
+    else: # Remove category_slug from the dict, otherwise "None" will be used at the category
+        paginator_previous_url = reverse("vcms.apps.simplenews.views.news_index", kwargs={ "page": news_paginator.previous_page_number() })
+        paginator_next_url = reverse("vcms.apps.simplenews.views.news_index", kwargs={ "page": news_paginator.next_page_number() })
+    return render_to_response("index.html", { "categories": categories, "contents": contents, "paginator": news_paginator, "paginator_previous_url": paginator_previous_url, "paginator_next_url": paginator_next_url }, context_instance=RequestContext(request))
 
 def news_unique(request, category_slug, news_slug):
     context = {}
