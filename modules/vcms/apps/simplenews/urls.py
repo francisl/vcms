6a8c80aaddc5f9e951538dacd6210b7a2d345067
Francois Lebel <francoislebel@gmail.com>
2010-03-31 12:45:10 -0400

    Fixed regex for a few urls.

--- modules/vcms/apps/simplenews/urls.py
+++ modules/vcms/apps/simplenews/urls.py
@@ -10,15 +10,15 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('vcms.apps.simplenews.views',
     (r'^news/(?P<category_slug>\w+)/page-(?P<page>\d+)/$', 'news_index'), # /news/finances/page-1/
     url(r'^news/page-(?P<page>\d+)/$', 'news_index', { "category_slug": None }), # /news/page-1/
-    (r'^news/(?P<category_slug>\w+)/(?P<news_slug>)/$', 'news_unique'), # /news/finances/year-of-the-linux-desktop/
-    (r'^news/(?P<category_slug>\w+)/category/(?P<category>\w)/$', 'news_category'), # /news/finances/category/Technology/
+    (r'^news/(?P<category_slug>\w+)/(?P<news_slug>\w+)/$', 'news_unique'), # /news/finances/year-of-the-linux-desktop/
+    (r'^news/(?P<category_slug>\w+)/category/(?P<category>\w+)/$', 'news_category'), # /news/finances/category/Technology/
     url(r'^news/category/(?P<category>\w)/$', 'news_category', { "category_slug": None }), # /news/category/Technology/
-    (r'^news/(?P<category_slug>\w+)/category/(?P<category>\w)/page-(?P<page>\d+)/$', 'news_category'), # /news/finances/category/Technology/page-1/
+    (r'^news/(?P<category_slug>\w+)/category/(?P<category>\w+)/page-(?P<page>\d+)/$', 'news_category'), # /news/finances/category/Technology/page-1/
     url(r'^news/category/(?P<category>\w)/page-(?P<page>\d+)/$', 'news_category', { "category_slug": None }), # /news/category/Technology/page-1/
-    #(r'^news/(?P<category_slug>\w+)/category/(?P<category>\w).atom$', 'TODO'), # /news/finances/category/Technology.atom
-    #url(r'^news/category/(?P<category>\w).atom$', 'TODO', { "category_slug": None }), # /news/category/Technology.atom
-    #(r'^news/(?P<category_slug>\w+)/category/(?P<category>\w).rss$', 'TODO'), # /news/finances/category/Technology.rss
-    #url(r'^news/category/(?P<category>\w).rss$', 'TODO', { "category_slug": None }), # /news/category/Technology.rss
+    #(r'^news/(?P<category_slug>\w+)/category/(?P<category>\w+).atom$', 'TODO'), # /news/finances/category/Technology.atom
+    #url(r'^news/category/(?P<category>\w+).atom$', 'TODO', { "category_slug": None }), # /news/category/Technology.atom
+    #(r'^news/(?P<category_slug>\w+)/category/(?P<category>\w+).rss$', 'TODO'), # /news/finances/category/Technology.rss
+    #url(r'^news/category/(?P<category>\w+).rss$', 'TODO', { "category_slug": None }), # /news/category/Technology.rss
     (r'^news/(?P<category_slug>\w+)/archives/(?P<month>\d{2})-(?P<year>\d{4})/$', 'news_archives'), # /news/finances/archives/12-2010/
     url(r'^news/archives/(?P<month>\d{2})-(?P<year>\d{4})/$', 'news_archives', { "category_slug": None }), # /news/archives/12-2010/
     (r'^news/(?P<category_slug>\w+)/archives/(?P<month>\d{2})-(?P<year>\d{4})/page-(?P<page>\d+)/$', 'news_archives'), # /news/finances/archives/12-2010/page-1/
