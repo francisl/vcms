026e68b8c03d33cd52874117183af486521be07f
Francois Lebel <francoislebel@gmail.com>
2010-04-05 10:49:56 -0400

    Added contect to SimpleNews views.

--- modules/vcms/apps/simplenews/views.py
+++ modules/vcms/apps/simplenews/views.py
@@ -11,9 +11,11 @@ from django.template import RequestContext
 from vcms.apps.simplenews import settings
 from vcms.apps.simplenews.models import News, NewsCategory
 from vcms.apps.simplenews.models import APP_SLUGS
+from vcms.apps.www.views import InitPage
 
 
-def news_index(request, category_slug, page=1):
+def news_index(request, category_slug, page=1, context={}):
+    context.update(locals())
     categories = NewsCategory.objects.all()
     if category_slug:
         news = News.objects.filter(category__slug=category_slug)
@@ -33,19 +35,23 @@ def news_index(request, category_slug, page=1):
     else: # Remove category_slug from the dict, otherwise "None" will be used at the category
         paginator_previous_url = reverse("vcms.apps.simplenews.views.news_index", kwargs={ "page": news_paginator.previous_page_number() })
         paginator_next_url = reverse("vcms.apps.simplenews.views.news_index", kwargs={ "page": news_paginator.next_page_number() })
-    return render_to_response("index.html", { "categories": categories, "contents": contents, "paginator": news_paginator, "paginator_previous_url": paginator_previous_url, "paginator_next_url": paginator_next_url }, context_instance=RequestContext(request))
+    context.update({ "categories": categories, "contents": contents, "paginator": news_paginator, "paginator_previous_url": paginator_previous_url, "paginator_next_url": paginator_next_url })
+    return render_to_response("index.html", context, context_instance=RequestContext(request))
 
-def news_unique(request, category_slug, news_slug):
-    context = {}
-    context.update(InitPage(page_slug=news_slug, app_slug=APP_SLUGS))
+def news_unique(request, category_slug, news_slug, context={}):
+    context.update(InitPage(page_slug=category_slug, app_slug=APP_SLUGS))
     context.update(locals())
-    if context["module"] in globals():
-        return render_to_response("", {}, context_instance=RequestContext(request))
-    else:
-        return HttpResponseRedirect("/")
+    context.update({ "categories": categories, "contents": contents, "paginator": news_paginator, "paginator_previous_url": paginator_previous_url, "paginator_next_url": paginator_next_url })
+    return render_to_response("index.html", context, context_instance=RequestContext(request))
 
-def news_category(request, category_slug, category, page=1):
-    return render_to_response("", {}, context_instance=RequestContext(request))
+def news_category(request, category_slug, category, page=1, context={}):
+    context.update(InitPage(page_slug=category_slug, app_slug=APP_SLUGS))
+    context.update(locals())
+    context.update({ "categories": categories, "contents": contents, "paginator": news_paginator, "paginator_previous_url": paginator_previous_url, "paginator_next_url": paginator_next_url })
+    return render_to_response("", context, context_instance=RequestContext(request))
 
-def news_archives(request, category_slug, month, year, page=1):
-    return render_to_response("", {}, context_instance=RequestContext(request))
+def news_archives(request, category_slug, month, year, page=1, context={}):
+    context.update(InitPage(page_slug=category_slug, app_slug=APP_SLUGS))
+    context.update(locals())
+    context.update({ "categories": categories, "contents": contents, "paginator": news_paginator, "paginator_previous_url": paginator_previous_url, "paginator_next_url": paginator_next_url })
+    return render_to_response("", context, context_instance=RequestContext(request))
