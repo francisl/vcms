4cf89f2728ab14c2c63852b5a25b40819a5c91c8
Francois Lebel <francoislebel@gmail.com>
2010-04-07 21:54:16 -0400

    Added settings and urls modules to SimpleAnnouncement, also includes the django.contrib.comments app dependency.

---
+++ modules/vcms/apps/simpleannouncements/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Application: Vimba - CMS
+# Module: SimpleAnnouncements
+# Copyright (c) 2010 Vimba inc. All rights reserved.
+# Created by Francois Lebel on 20-03-2010.
+
+
+INSTALLED_APPS = (
+    'django.contrib.comments', # http://docs.djangoproject.com/en/dev/ref/contrib/comments/
+    'django.contrib.sites', # Required by django.contrib.comments
+    'tagging', # http://code.google.com/p/django-tagging/
+)
