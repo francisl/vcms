46e2dbec34c8d204631896832402bf091e826b34
Frank Lavoie <lavoie.francis@vimba.ca>
2010-04-15 15:30:21 -0400

    change import for tree.helper

--- modules/vimba_cms_simthetiq/apps/products/managers.py
+++ modules/vimba_cms_simthetiq/apps/products/managers.py
@@ -187,10 +187,10 @@ class ProductPageManager(models.Manager):
 
 class CompactNavigationGroupManager(models.Manager): 
     def get_navigation(self):
-        from vcms.apps.vwm import tree
+        from vcms.apps.vwm.tree import helper
         """ return navigation tree in a dictionary structure """ 
         menu = []
         for navgroup in self.all():
-            menu.append(tree.helper.create_tree_structure(navgroup.name, navgroup.get_absolute_url()))
+            menu.append(helper.create_tree_structure(navgroup.name, navgroup.get_absolute_url()))
         return menu
 
