1d2c29853a82bb7b966f1bda97025503c28bdf08
Frank Lavoie <lavoie.francis@vimba.ca>
2010-05-19 15:02:04 -0400

    satchmo install script take --home variable into account

--- modules/vcms/tools/install_dependencies.sh
+++ modules/vcms/tools/install_dependencies.sh
@@ -17,47 +17,49 @@ case $1 in
         ;;
 esac
 
-python_cmd=python$python_version
+homedir='/home/simuser'
+
+python_cmd=python$python_version setup.py install --home=$homedir
 easy_install_cmd=easy_install-$python_version
 
 
 # install SATCHMO dependencies
 echo "-----------PyCRYPTO---------"
-easy_install pycrypto
+$easy_install_cmd pycrypto
 echo "-----------REPORTLAB---------"
-easy_install reportlab
+$easy_install_cmd reportlab
 echo "-----------TRML2PDF---------"
-easy_install http://www.satchmoproject.com/snapshots/trml2pdf-1.2.tar.gz
+$easy_install_cmd http://www.satchmoproject.com/snapshots/trml2pdf-1.2.tar.gz
 echo "-----------DJANGO-REGISTRATION---------"
-easy_install django-registration
+$easy_install_cmd django-registration
 echo "-----------PyYAML---------"
-easy_install PyYAML
+$easy_install_cmd PyYAML
 
 echo "-----------DJANGO-THREADED-MULTIHOST---------"
 hg clone https://bkroeze@bitbucket.org/bkroeze/django-threaded-multihost/ django-threaded-multihost
 cd django-threaded-multihost
-$python_cmd setup.py install
+$python_cmd
 cd ..
 rm -rf django-threaded-multihost/
 
 echo "-----------DJANGO-APP-PLUGINS---------"
 svn checkout http://django-app-plugins.googlecode.com/svn/trunk/ django-app-plugins-read-only
 cd django-app-plugins-read-only
-$python_cmd setup.py install
+$python_cmd
 cd ..
 rm -rf django-app-plugins-read-only/
 
 echo "-----------DJANGO-SIGNALS-AHOY---------"
 hg clone http://bitbucket.org/bkroeze/django-signals-ahoy/ django-signals-ahoy
 cd django-signals-ahoy
-$python_cmd setup.py install
+$python_cmd
 cd ..
 rm -rf django-signals-ahoy/
 
 echo "-----------DJANGO-LIVESETTINGS---------"
 hg clone http://bitbucket.org/bkroeze/django-livesettings django-livesettings
 cd django-livesettings
-$python_cmd setup.py install
+$python_cmd
 cd ..
 rm -rf django-livesettings/
 
@@ -71,11 +73,9 @@ rm -rf django-keyedcache/
 echo "-----------SATCHMO---------"
 hg clone http://bitbucket.org/chris1610/satchmo/ satchmo-trunk
 cd satchmo-trunk
-$python_cmd setup.py install
+$python_cmd
 cd ..
 rm -rf satchmo-trunk
-
-
 
 
 # OPTIONAL FOR DOCUMENTATION
