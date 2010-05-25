f433d5a0e2cf74d81ada0b19abbffa8285f5f24d
Frank Lavoie <lavoie.francis@vimba.ca>
2010-05-24 21:53:46 -0400

    add url for contact

--- modules/vcms/apps/contact/urls.py
+++ modules/vcms/apps/contact/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('vcms.apps.contact.views',
-    (r'page/(?P<page>[-\w]+)/$', 'Contact'),    
+    (r'page/(?P<page>[-\w]+)/$', 'Contact'),
+    (r'', 'Contact'),
 )
