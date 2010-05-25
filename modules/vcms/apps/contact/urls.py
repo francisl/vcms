d76d635911e4bc1d71d244be2b1c612104c1a5d5
Frank Lavoie <lavoie.francis@vimba.ca>
2010-05-25 12:03:48 -0400

    remove bug in contact that trigger calls to Initpage

--- modules/vcms/apps/contact/urls.py
+++ modules/vcms/apps/contact/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('vcms.apps.contact.views',
-    (r'page/(?P<page>[-\w]+)/$', 'Contact'),
-    (r'', 'Contact'),
+    (r'^page/(?P<page>[-\w]+)/$', 'Contact'),
+    (r'^$', 'Contact'),
 )
