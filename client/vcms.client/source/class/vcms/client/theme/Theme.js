1f2a24263ab8432a2a99c9446703d68c935e95e1
Francois Lebel <francoislebel@gmail.com>
2010-06-08 18:55:15 -0400

    Initial Qooxdoo vcms.client project import.

---
+++ client/vcms.client/source/class/vcms/client/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("vcms.client.theme.Theme",
+{
+  meta :
+  {
+    color : vcms.client.theme.Color,
+    decoration : vcms.client.theme.Decoration,
+    font : vcms.client.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : vcms.client.theme.Appearance
+  }
+});
