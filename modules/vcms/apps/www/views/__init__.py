9957cc56b99db6a6f6882379ec7b91169ed687fc
Frank Lavoie <lavoie.francis@vimba.ca>
2010-06-08 22:28:14 -0400

    add ajax url/views to get pages list

---
+++ modules/vcms/apps/www/views/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF8 -*-
+
+from vcms.apps.www.views.ajax import *
+from vcms.apps.www.views.html import *
---
+++ modules/vcms/apps/www/views/ajax.py
@@ -0,0 +1,64 @@
+# encoding: utf-8
+# copyright Vimba inc. 2010
+# programmer : Francis Lavoie
+
+from django.utils import simplejson
+from django.core import serializers
+from django.contrib.auth.decorators import login_required
+#from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+from vcms.apps.www.models.page import BasicPage
+from vcms.apps.www.models.menu import MainMenu as mm
+
+@login_required
+def get_page_list(request):
+    """ return a json dictionnay of all available page
+        @return: ordered dictionnary
+            menu_id : { id : int
+                        ,menu_name : string
+                        ,lft : int (element on the left)
+                        ,rgt : int (element on the right)
+                        ,slug : string
+                        ,description : string
+                        ,containers : { 
+                            container_id : { name : string
+                                            , type : string
+                                            }
+                                    }
+                        ,pages: { (recursive) }
+                        ,display_in_menu : boolean
+                        ,published: int (0 = draft, 1=published)
+                    }
+    """
+    
+    if request.method == 'GET':
+        pages = BasicPage.objects.get_all_basic()
+        mainmemu = mm.objects.get(menu_name='english')
+        print("all basic page = %s" % pages)
+        pages_dict = {}
+
+        for page in mainmemu.get_children():
+            pages_dict[page.id] = {"id" : page.content_object.id
+                                   ,"left" : page.lft
+                                   ,"right" : page.rgt
+                                   ,"menu_name" : page.menu_name
+                                   ,"slug" : page.content_object.slug
+                                   ,"description": page.content_object.description
+                                   ,"containers" : {}
+                                   ,"pages" : {}
+                                   ,"displayed" : page.display
+                                   ,"published" : page.content_object.status
+                                   }
+        print("dict to json : %s " % pages_dict)
+        return HttpResponse(simplejson.dumps(pages_dict), mimetype='application/javascript')
+        
+
+@login_required
+def add_new_page(request):
+    pass
+
+@login_required
+def update_page(request):
+    pass
+
