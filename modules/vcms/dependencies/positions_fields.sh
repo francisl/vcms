bcae5ab79869e68f13b4a130a7f3eef009abbe45
Frank Lavoie <lavoie.francis@vimba.ca>
2010-07-28 21:39:12 -0400

    add installation script for django-position

---
+++ modules/vcms/dependencies/positions_fields.sh
@@ -0,0 +1,5 @@
+. ./dep_config.sh
+
+echo "----------- BEAUTIFUL-SOUP ---------"
+sudo pip install -e git+git://github.com/jpwatts/django-positions.git#egg=django-position
+sudo easy_install-2.5 ~/src/django-position/
