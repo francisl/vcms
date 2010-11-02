a37ad7433c1d914a971e1d372cffb0f120fab665
Frank Lavoie <lavoie.francis@vimba.ca>
2010-11-01 20:27:45 -0400

    move simthetiq module into its own repo

--- modules/vimba_cms_simthetiq/apps/order/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from django.conf import settings
-
-urlpatterns = patterns('vimba_cms_simthetiq.apps.order.views',
-        (r'^$', 'Order'),
-        (r'orderform/$', 'OrderForm'),
-        #(r'^validation/$', 'validation'),
-        (r'confirmation/$', 'Confirm'),
-)
